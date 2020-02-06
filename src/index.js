36a5d1ebc0a738329efd7f7f72bc288cd03acbd5
James Hollenbeck <james@jhollen.com>
2020-02-06 14:45:09 -0500

    Fix for sw.js Style fixes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import App from './ncounter/App.svelte';
 import runtime from 'serviceworker-webpack-plugin/lib/runtime';
+import 'reset-css/reset.css';
+import './ncounter/site.css';
 
 if ('serviceWorker' in navigator) {
     const registration = runtime.register();
