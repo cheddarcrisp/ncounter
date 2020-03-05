cea4f35d521067624af99a3b6a2c9a746f25c4bf
CheddarCrisp <cheddar@crispbyte.dev>
2020-03-04 20:53:18 -0500

    Add validation to forms Fix values being treated as strings

--- src/ncounter/App.svelte
+++ src/ncounter/App.svelte
@@ -11,14 +11,16 @@
 </div>
 {#if showAddDialog}
 <div class="dialog">
-    <div class="dialog-content">
-        <label>Name<input type="text" bind:value={newCounter.title} use:focus/></label>
-        <label>Initial Value<input type="text" inputmode="decimal" bind:value={newCounter.initialValue} /></label>
-        <label>Target<input type="text" inputmode="decimal" bind:value={newCounter.max} /></label>
+    <form class="dialog-content" novalidate bind:this={ dialogForm }>
+        <label>Counter Name<input type="text" required bind:value={newCounter.title} use:focus/></label>
+        <label>Initial Value<input type="text" required pattern="(0|([1-9]\d*))(\.\d+)?" inputmode="decimal" bind:value={newCounter.initialValue} /></label>
+        <label>Target<input type="text" pattern="(0|([1-9]\d*))(\.\d+)?" inputmode="decimal" bind:value={newCounter.max} /></label>
         <label>Save history<input type="checkbox" bind:checked={newCounter.saveHistory} /></label>
-        <button on:click={ () => { showAddDialog = false; } }>Cancel</button>
-        <button class="ok" on:click={ add }>OK</button>
-    </div>
+        <div class="buttons">
+            <button class="ok" on:click|preventDefault={ add }>OK</button>
+            <button on:click|preventDefault={ () => { showAddDialog = false; } }>Cancel</button>
+        </div>
+    </form>
 </div>
 {/if}
 {#if showAbout}
@@ -32,6 +34,7 @@ import { counters } from './db.js';
 let showAddDialog = false;
 let showAbout = false;
 let newCounter = null;
+let dialogForm = null;
 
 function focus(node){
     node.focus();
@@ -41,6 +44,7 @@ function showAdd(){
     newCounter = {
         title: '',
         initialValue: 0,
+        max: '',
         saveHistory: false,
         history: []
     }
@@ -49,9 +53,15 @@ function showAdd(){
 }
 
 function add(){
+    if(!dialogForm.checkValidity()) {
+        dialogForm.removeAttribute('novalidate');
+        return;
+    }
+
     const counter = {
         ...newCounter,
-        value: newCounter.initialValue
+        max: newCounter.max !== '' ? +newCounter.max : null,
+        value: +newCounter.initialValue
     }
 
     counters.add(counter);
@@ -60,10 +70,6 @@ function add(){
 }
 </script>
 <style lang="scss">
-    .ok {
-        margin-left: auto;
-    }
-
     .app {
         display: flex;
         flex-direction: column;
