cf793cc89550fec2e63b5bf182b334aa3ecc4239
CheddarCrisp <cheddar@crispbyte.dev>
2020-03-13 21:35:35 -0400

    Create UI to prompt for reload when an update is available

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,18 @@
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const Terser = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production'),
+        }),
+    ],
+    optimization:{
+        minimizer: [new Terser({
+            test: /\.m?js$/,
+        })]
+    }
 });
