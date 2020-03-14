7fa7dd4e0f5ee59a57740b55ce5d36f8830fa883
CheddarCrisp <cheddar@crispbyte.dev>
2020-03-13 21:35:35 -0400

    Create UI to prompt for reload when an update is available

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import App from './ncounter/App.svelte';
-import runtime from 'serviceworker-webpack-plugin/lib/runtime';
 import 'reset-css/reset.css';
 import './ncounter/site.css';
+import {Workbox} from 'workbox-window/Workbox.mjs';
+
+const app = new App({ target: document.body });
 
 if ('serviceWorker' in navigator) {
-    const registration = runtime.register();
-}
+    const wb = new Workbox('sw.js');
 
-new App({ target: document.body });
+    wb.addEventListener('message', async (event) => {
+        if (event.data.type === 'CACHE_UPDATED') {
+            app.SetUpdateAvailable();
+        }
+    });
+
+    wb.register();
+}
